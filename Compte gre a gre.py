4c8330a065574fab707d0c7680325e6b47bae3bf
chris4411 <57400495+chris4411@users.noreply.github.com>
2019-11-10 15:09:57 +0000

    Update Compte gre a gre.py

--- Compte gre a gre.py
+++ Compte gre a gre.py
@@ -1,25 +1,24 @@
-class compteGG:
+﻿class compteGG:
 
-        def __init__(self,mt = 0.0,solde = 0.0):
-        self._mt = m
-        self._solde = solde
-        
-    def __str__(self):
-         return "Solde disponible : {0}".format(self.solde)
+    def __init__(self):
+        self.mt = 0.0
+        self.solde = 0.0
 
 
     def infoSolde(self):
-        print("Votre solde est de {0} FCFA ! ".format(self._solde))
+        print("Votre solde est de {0} FCFA ! ".format(self.solde))
 
     def versement(self,mt):
-        self._solde += mt
+        self.solde += mt
         print("Versement de {0} FCFA éffectué !".format(mt))
         self.infoSolde()
     
     def retrait(self,mt):
-        if(mt>solde):
+        if(mt>self.solde):
             print("Solde insuffisant")
         else:
-            self._solde -= mt
+            self.solde -= mt
             print("Le montant débité est de {0} FCFA".format(mt))
         self.infoSolde()
+ch = compteGG()
+ch.versement(30000)
